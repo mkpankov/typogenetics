eac778a09c55664ed9034c901b0c2125020e5a02
Michael Pankov <constantius9@gmail.com>
2012-10-26 01:06:05 +0400

    Added exceptions on creation and __repr__ and __str__

--- src/strand.py
+++ src/strand.py
@@ -1,1 +1,23 @@
-bases = set(['A', 'C', 'G', 'T'])
+bases = frozenset(['A', 'C', 'G', 'T'])
+
+class IncorrectString(ValueError):
+    pass
+
+class NotAString(TypeError):
+    pass
+
+class Strand:
+    def __init__(self, string):
+        if isinstance(string, str):
+            if frozenset(string) != bases:
+                raise IncorrectString
+        else:
+            raise NotAString
+
+        self.contents = string
+
+    def __repr__(self):
+        return 'Strand("{0}")'.format(self.contents)
+
+    def __str__(self):
+        return 'Strand("{0}")'.format(self.contents)
--- src/strandtest.py
+++ src/strandtest.py
@@ -6,10 +6,29 @@ import strand
 import unittest
 
 class BasesCheck(unittest.TestCase):
-    correct_bases = set(['A', 'C', 'G', 'T'])
+    correct_bases = frozenset(['A', 'C', 'G', 'T'])
     def test(self):
         """It should be just that set"""
         self.assertEqual(self.correct_bases, strand.bases)
+
+class StrandTest(unittest.TestCase):
+    correct_bases = frozenset(['A', 'C', 'G', 'T'])
+    def testCorrectCreation(self):
+        """Strand should contain correct string"""
+        s = strand.Strand('ACGT')
+        self.assertEqual('ACGT', s.contents)
+    def testIncorrectCreationBadString(self):
+        """Strand should raise an exception in case of bad string"""
+        self.assertRaises(strand.IncorrectString, strand.Strand, 'BCDE')
+    def testIncorrectCreationNotAString(self):
+        """Strand should raise an exception in case of not a string"""
+        self.assertRaises(strand.NotAString, strand.Strand, 42)
+    def testRepr(self):
+        s = strand.Strand('ACGT')
+        self.assertEqual('Strand("ACGT")', repr(s))
+    def testStr(self):
+        s = strand.Strand('ACGT')
+        self.assertEqual('Strand("ACGT")', str(s))
 
 if __name__ == "__main__":
     unittest.main()
