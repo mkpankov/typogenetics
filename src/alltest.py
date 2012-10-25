851912bbcf71f85d533635f8b49c0c93ff561b20
Michael Pankov <constantius9@gmail.com>
2012-10-26 01:41:47 +0400

    Added a script for total testing

---
+++ src/alltest.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+"""Testing script which invokes all sub-tests"""
+
+from strandtest import *
+from enzymetest import *
+import unittest
+
+if __name__ == "__main__":
+    unittest.main()
