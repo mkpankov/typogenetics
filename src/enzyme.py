4281e00cc31243624b83650270cf89b51fb4799b
Michael Pankov <constantius9@gmail.com>
2012-10-26 02:18:33 +0400

    Added aminoacids and corresponding checks to enzyme

--- src/enzyme.py
+++ src/enzyme.py
@@ -1,3 +1,39 @@
+aminoacids = frozenset(
+    [   
+        'cut', # cut strand(s)
+        'del', # delete base from strand
+        'swi', # switch enzyme to other strand
+        'mvr', # move one unit to the right
+        'mvl', # move one unit to the left
+        'cop', # turn one Copy mode
+        'off', # turn off Copy mode
+        'ina', # insert A to the right of this unit
+        'inc', # insert C to the right of this unit
+        'ing', # insert G to the right of this unit
+        'int', # insert T to the right of this unit
+        'rpy', # search for the nearest pyrimidine to the right
+        'rpu', # search for the nearest purine to the right
+        'lpy', # search for the nearest pyrimidine to the left
+        'lpu', # search for the nearest purine to the left
+    ]
+)
+
+class NotInSet(ValueError):
+    pass
+
+class NotAString(TypeError):
+    pass
+
 class Enzyme:
-	"""It's a machine operating on strands by means of instructions--Amino Acids"""
-	pass
+    """It's a machine operating on strands by means of instructions--Amino Acids"""
+    def __init__(self, commands):
+        try:
+            for item in commands:
+                if not isinstance(item, str):
+                    raise NotAString
+                else:
+                    if item not in aminoacids:
+                        raise NotInSet
+        except TypeError:
+            raise
+        self.commands = commands
--- src/enzymetest.py
+++ src/enzymetest.py
@@ -5,12 +5,36 @@
 import enzyme
 import unittest
 
+class AminoAcidsCheck(unittest.TestCase):
+    def testAminoAcidsSet(self):
+        """Aminoacids is fixed set of commands"""
+        aminoacids = frozenset(
+            [
+                'cut', 'del', 'swi', 'mvr', 'mvl', 'cop', 'off', 
+                'ina', 'inc', 'ing', 'int', 'rpy', 'rpu', 'lpy', 'lpu', 
+            ]
+        )
+        self.assertEquals(enzyme.aminoacids, aminoacids)
+
 class EnzymeCheck(unittest.TestCase):
-	def testCorrectCreation(self):
-		"""Enzyme is created from an iterable with commands"""
-		commands = ['cut', 'del', 'swi']
-		e = enzyme.Enzyme(commands)
-		self.assertEquals(e.commands, commands)
+    def testCorrectCreation(self):
+        """Enzyme is created from an iterable with commands"""
+        commands = ['cut', 'del', 'swi']
+        e = enzyme.Enzyme(commands)
+        self.assertEquals(e.commands, commands)
+    def testTotalCreation(self):
+        """Enzyme is created with all possible commands"""
+        commands = ['cut', 'del', 'swi', 'mvr', 'mvl', 'cop', 'off',
+                    'ina', 'inc', 'ing', 'int', 'rpy', 'rpu', 'lpy', 'lpu']
+        e = enzyme.Enzyme(commands)
+        self.assertEquals(e.commands, commands)
+    def testIncorrectCreationStringNotInSet(self):
+        self.assertRaises(enzyme.NotInSet, enzyme.Enzyme, ['aaa'])
+    def testIncorrectCreationNotAString(self):
+        self.assertRaises(enzyme.NotAString, enzyme.Enzyme, [42])
+    def testIncorrectCreationNotIterable(self):
+        self.assertRaises(TypeError, enzyme.Enzyme, 42)
+
 
 if __name__ == "__main__":
     unittest.main()
