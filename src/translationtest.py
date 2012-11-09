040fa9bff1628d62ce346b396368c29bbc19f195
Michael Pankov <constantius9@gmail.com>
2012-11-10 00:42:40 +0400

    Started rewrite from scratch

--- src/translationtest.py
+++ src/translationtest.py
@@ -2,63 +2,15 @@
 
 """Unit test for translation"""
 
-import strand
-import enzyme
+
 import unittest
-import itertools
+import tranlation
 
 
-class TranslationTest(unittest.TestCase):
-    def testASimplestTranslation(self):
-        """Strand is translated by enzyme to produce set of new strands"""
-        string = 'ACA'
-        s = strand.Strand(string)
-        e = enzyme.Enzyme(['dlt', 'mvr', 'itt'], 'A')
-        locus = 0
-        e.attach(s, locus)
-        result = next(e.translate())
-        self.assertEquals(result, [list('CAT')])
-
-    def testASimpleTranslation(self):
-        string = 'TAGATCCAGTCCATCGA'
-        s = strand.Strand(string)
-        e = enzyme.Enzyme(['rpu', 'inc'], 'G')
-        locus = 1
-        e.attach(s, locus)
-        self.assertEquals(e.locus, 8)
-        result = next(e.translate())
-        self.assertEquals(result, [list('TAGATCCAGTCCACTCGA')])
-
-    def testASimpleCopyingTranslation(self):
-        string = 'ACGT'
-        s = strand.Strand(string)
-        e = enzyme.Enzyme([
-            'cop', 'rpy', 'rpy', 'mvl', 'mvl', 'mvl', 'off', 'ina'],
-            'A')
-        locus = 0
-        e.attach(s, locus)
-        result = next(e.translate())
-        self.assertEquals(result, [list('AACGT'), list('T GCA')])
-
-    def testCorrectTranslation(self):
-        """Strand is translated by enzyme to produce set of new strands"""
-        string = 'TAGATCCAGTCCATCGA'
-        s = strand.Strand(string)
-        e = enzyme.Enzyme([
-            'rpu', 'inc', 'cop', 'mvr', 'mvl', 'swi', 'lpu', 'itt'],
-            'G')
-        locus = 1
-        e.attach(s, locus)
-        self.assertEquals(e.locus, 8)
-        result = next(e.translate())
-        self.assertEquals(result,
-            [list('AGCTACACCTGACCTAGAT'), list('   ATG             ')])
-
-    def testNotAttachedTranslation(self):
-        """When enzyme isn't attached, it can't translate"""
-        e = enzyme.Enzyme(['rpu'], 'A')
-        i = e.translate()
-        self.assertRaises(enzyme.NotAttached, next, i)
+class TranslationCheck(unittest.TestCase):
+    def test_simple_translation_result(self):
+        pass
+        
 
 if __name__ == "__main__":
     unittest.main()
